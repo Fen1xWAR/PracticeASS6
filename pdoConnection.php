3e3b9371dfd3219c414f8fa2f8edbbd93643dcfa
Fen1xWAR <proanvl@gmail.com>
2024-04-16 23:58:29 +0300

    Обновил PDO до внешней базы

--- pdoConnection.php
+++ pdoConnection.php
@@ -2,7 +2,7 @@
 $user = 'root';
 $pass = '2203';
 try {
-    $dbh = new PDO('mysql:host=localhost;dbname=education_system', $user, $pass);
+    $dbh = new PDO('mysql:host=94.143.46.65;dbname=education_system', $user, $pass);
 } catch (Exception $e) {
     echo 'Error: ',  $e->getMessage(), "\n";
 }
